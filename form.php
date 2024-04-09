8faa140e5aec6b2b3927a740ab0be973136c0896
JalalProHacker <130361739+JalalProHacker@users.noreply.github.com>
2024-04-09 10:55:29 +0100

    Update form.php

--- form.php
+++ form.php
@@ -3,8 +3,8 @@
     
   </head>
   <body>
-    <h1>Form.php</h1>
-    <h2>Username entered: <?php echo $_GET["username"];?></h2>
-    <h2>Password entered: <?php echo $_GET["password"];?></h2>
+    <h1>Form.php using post method</h1>
+    <h2>Username entered: <?php echo $_POST["username"];?></h2>
+    <h2>Password entered: <?php echo $_POST["password"];?></h2>
   </body>
 </html>
