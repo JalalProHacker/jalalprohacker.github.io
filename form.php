3b6aed4cd32b7ce3479e2284f148a1353c502e7c
JalalProHacker <130361739+JalalProHacker@users.noreply.github.com>
2024-04-09 10:37:47 +0100

    Create form.php

---
+++ form.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <h1>Form.php</h1>
+    <h2>Username entered: <?php echo $_POST["username"];?></h2>
+    <h2>Password entered: <?php echo $_POST["password"];?></h2>
+  </body>
+</html>
